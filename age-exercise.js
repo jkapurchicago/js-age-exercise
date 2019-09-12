704b29681c5f111598fd0a4910f3a0a49ecaaff6
Jatin Kapur <jkapur.work@gmail.com>
2019-09-12 11:19:09 -0500

    initial commit, learning prompts and alerts on javascript

---
+++ age-exercise.js
@@ -0,0 +1,9 @@
+var age = prompt("How old are you");  
+days = age * 365.25; 
+alert(age + " years is roughly " + days + "days");    
+
+
+
+apple = (NaN == NaN);
+console.log(apple); 
+null == defined 
